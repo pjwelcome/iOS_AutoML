77024a1df004e16615c2b1555e5824802636e4c3
Peter-John Welcome <pjwelcome@gmail.com>
2019-10-28 11:27:29 +0200

    Initial Commit

---
+++ AutoMLiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoMLiOS
+//
+//  Created by pjapple on 2019/10/28.
+//  Copyright © 2019 DVT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
